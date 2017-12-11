dc12aef6150e90290e7abfe163a390c3d2d303ed
jemal <jemal@LAPTOP-9MQ2DLFB.home>
2017-12-11 20:19:21 +0100

    some more modificaitons for Hausarztkontakt_presenter

--- Tepath/src/main/java/com/gluonapplication/views/HausarztkontaktPresenter.java
+++ Tepath/src/main/java/com/gluonapplication/views/HausarztkontaktPresenter.java
@@ -7,43 +7,23 @@ import com.gluonhq.charm.glisten.control.AppBar;
 import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 
 public class HausarztkontaktPresenter  {
 	
 
     @FXML
-    private View hausarzt;
-//    @FXML
-//    private Button Zurück;
-//
-//    @FXML
-//    void ButtonSendenClick(ActionEvent event) {
-//
-//    }
-//
-//    @FXML
-//    void ButtonZurückKlick(ActionEvent event) {
-//    	Zurück.setOnAction(e -> 
-//        MobileApplication.getInstance().switchView(GluonApplication.KONTAKTE_VIEW));
-      	
-   // }
+    private View hausarztkontakt;
 
 
-//    protected void updateAppBar(AppBar appBar) {
-//        appBar.setNavIcon(MaterialDesignIcon.MENU.button(e -> switchView(SECONDARY_VIEW)));
-//        appBar.setTitleText("Hausarzt");
-//    }
     public void initialize() {
-    	hausarzt.setShowTransitionFactory(BounceInRightTransition::new);
-    	hausarzt.showingProperty().addListener((obs, oldValue, newValue) -> {
+    	hausarztkontakt.setShowTransitionFactory(BounceInRightTransition::new);
+    	hausarztkontakt.showingProperty().addListener((obs, oldValue, newValue) -> {
             if (newValue) {
                 AppBar appBar = MobileApplication.getInstance().getAppBar();
                 appBar.setNavIcon(MaterialDesignIcon.MENU.button(e -> 
                         MobileApplication.getInstance().showLayer(GluonApplication.MENU_LAYER)));
-                appBar.setTitleText("Hausarzt");
+                appBar.setTitleText("Hausarztkontakt");
                 appBar.getActionItems().add(MaterialDesignIcon.SEARCH.button(e -> 
                         System.out.println("Search")));
             }
