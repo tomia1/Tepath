c3f289c762d001ce734c58671e466c3bcc155f81
jemal <jemal@LAPTOP-9MQ2DLFB.bfh.ch>
2017-12-27 14:12:52 +0100

    local articacts

--- Tepath/src/android/android/AndroidSMSService.java
+++ Tepath/src/android/android/AndroidSMSService.java
@@ -1,17 +1,19 @@
 package com.gluonhq.charm.down.plugins.android;
+
 import android.content.Intent;
 import android.net.Uri;
 import com.gluonhq.charm.down.plugins.SMSService;
 import javafxports.android.FXActivity;
+
 public class AndroidSMSService implements SMSService {
-	 @Override
-	    public void sendSMS(String number) {
-	        Intent smsIntent = new Intent(Intent.ACTION_VIEW);         
-	        smsIntent.setData(Uri.parse("sms:"));
-	        smsIntent.putExtra("address", number);
-	        FXActivity.getInstance().startActivity(smsIntent);
-	        
-	        Services.get(SMSService.class)
-            .ifPresent(s -> button.setOnAction(e -> s.sendSMS(textField.getText())));
-	    }
+	@Override
+	public void sendSMS(String number) {
+		Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+		
+		smsIntent.setData(Uri.parse("sms:"));
+		smsIntent.putExtra("address", number);
+		FXActivity.getInstance().startActivity(smsIntent);
+
+		Services.get(SMSService.class).ifPresent(s -> button.setOnAction(e -> s.sendSMS(textField.getText())));
+	}
 }
